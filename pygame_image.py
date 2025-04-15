61081b1920321c0bea9173c8c9fdd798029f3f7b
Warabi <c0a24282e6@edu.teu.ac.jp>
2025-04-15 16:02:22 +0900

    練習8: 背景画像を反転させてくっつけた

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    flipped_bg_img = pg.transform.flip(bg_img, True, False)
     kouka_img = pg.image.load("fig/3.png")
     kouka_img = pg.transform.flip(kouka_img, True, False)
     tmr = 0
@@ -17,12 +18,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr 
+        x = tmr
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(flipped_bg_img, [-x+1600, 0])
         screen.blit(kouka_img, [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200)
 
 
